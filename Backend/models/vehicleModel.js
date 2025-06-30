e02e50f3439699734deb58cac13e0edeb80fa86b
Charitha2937 <charitharayaluthirumalasetty@gmail.com>
2025-06-30 23:19:21 +0530

    DB connected

---
+++ Backend/models/vehicleModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const vehicleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  model: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, required: true },
+  mileage: { type: Number },
+  type: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model('Vehicle', vehicleSchema);
